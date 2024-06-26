73ce01c104c290d834092a9068eef29d41c727c9
Wiechmann-IT <164408737+Wiechmann-IT@users.noreply.github.com>
2024-06-26 14:55:16 +0200

    wp-config with hard credentials

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -1,28 +1,33 @@
 <?php
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv('WORDPRESS_DB_NAME') );
+define('DB_NAME', 'sql7716242');
 
 /** MySQL database username */
-define( 'DB_USER', getenv('WORDPRESS_DB_USER') );
+define('DB_USER', 'sql7716242');
 
 /** MySQL database password */
-define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD') );
+define('DB_PASSWORD', 'v9tavbHDSg');
 
 /** MySQL hostname */
-define( 'DB_HOST', getenv('WORDPRESS_DB_HOST') );
+define('DB_HOST', 'sql7.freesqldatabase.com');
 
 /** Database Charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8' );
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define('DB_COLLATE', '');
+
 /**
  * WordPress Database Table prefix.
  *
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
  */
-$table_prefix  = 'wp_2_';
+$table_prefix = 'wp_2_';
 
-// ... weitere Konfigurationen
+// Abschalten der Fehleranzeige, außer Sie wollen sie für die Fehlersuche aktivieren
+define('WP_DEBUG', false);
+
+// Weitere Konfigurationen...
+
